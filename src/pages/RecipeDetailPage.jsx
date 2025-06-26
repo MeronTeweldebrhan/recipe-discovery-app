0d5557329a75b6c365b817d7d0aebc82eaedcf97
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-25 22:21:10 -0400

    Create Usehook file and add  functionaltiy to fetch recipes

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -1,11 +1,25 @@
-
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/UseFetch";
 
 function RecipeDetailPage() {
+    const {id} =useParams()
+const { data, loading, error } = useFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+
+
+if(error) return <h2>{error.message}</h2>
+
+
+const meal =data?.meals[0]
+if(loading || !meal ) return <h2 className="text-5xl animate-bounce"> loading...</h2>
+
   return (
     <div>
-      <h1>Recipe Detail Page</h1>
-      <p>This is where the recipe details will be displayed.</p>
+        
+        {meal.strInstructions}
+        <img className="w-50 rounded flex justify-center" src={meal.strMealThumb}/>
+<button className="bg-"> Add to Fav</button>
     </div>
+    
   );
 }
 export default RecipeDetailPage;
