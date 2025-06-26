114b4c223e59d29ff6ccaced960cc48c6992dabe
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-25 22:22:30 -0400

    Fetch data and redering it

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,10 +1,60 @@
-
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function CategoryPage() {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { categoryName } = useParams();
+
+  useEffect(() => {
+    async function CategoryList() {
+      try {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+
+        setMeals(data.meals || []); // Use meals or an empty array if undefined
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.error("  fetch Problem:", error);
+      }
+    }
+    CategoryList();
+  }, [categoryName]); // Fetch categories when idMeal changes
+
+  useEffect(() => {
+    if (meals.length > 0) {
+      setLoading(false);
+    }
+  }, [meals]);
+
   return (
     <div>
-      <h1>Category Page</h1>
-      <p>This is the category page where you can explore recipes by category.</p>
+      <h1 className="text-2xl font-bold text-center mt-6">Category ↔ {categoryName}</h1>
+      {loading ? (
+        <p>Loading meals...</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 w-11/12 mx-auto mt-4 mb-4 text-center rounded-lg">
+          {meals.map((meal) => (
+            <NavLink key={meal.idMeal} to={`/recipe/${meal.idMeal}`}>
+            
+              <img
+                src={meal.strMealThumb}
+                className="r"
+                alt={meal.strMeal}
+              />
+
+              <h5 className="font-bold">{meal.strMeal}</h5>
+            </NavLink>
+          ))}
+        </div>
+      )}
+     
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,59 @@
-
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-   
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCategories(data.categories);
+        console.log(data);
+      } catch (error) {
+        console.error(" Fetch error:", error);
+      } finally{
+        setLoading(false)
+      }
+    }
+    fetchCategories();
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
+
+
   return (
-    <div className="color-scheme home-page bg-light text-dark p-4 rounded shadow ">
-      <h1>Welcome to the Recipe Discovery App</h1>
-      <p>Discover and share your favorite recipes.</p>
-    </div>
+    <li>
+      <h1 className="text-3xl font-bold text-center mt-4">
+        Welcome to the Recipe Discovery App
+      </h1>
+      {loading ? (
+        <div className="text-center mt-4">
+          <p>Loading recipes...</p>
+        </div>
+      ) : (
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 w-11/12 mx-auto mt-4 mb-4 text-center rounded-lg">
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <NavLink key={category.idCategory} to={`/category/${category.strCategory}`}>
+                <img
+                  src={category.strCategoryThumb}
+                  alt={category.strCategory}
+                />
+                <h5 className="font-bold">{category.strCategory}</h5>
+                
+              </NavLink>
+            ))}
+        </div>
+      )}
+    </li>
   );
 }
+
 export default HomePage;
