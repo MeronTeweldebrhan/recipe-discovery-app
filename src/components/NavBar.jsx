566852202a78e588e00b80a34167b7503fbc36a6
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-25 22:23:44 -0400

    Add styling with active inactive methods

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,18 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/favorites">Favorites</NavLink></li>
+    <nav className='bg-gray-900 text-white-shadow-md'>
+      <ul className='flex  space-x-8 '>
+        <li className='ms-10' ><NavLink 
+        className={({ isActive }) =>
+              isActive ? 'text-yellow-400 font-bold' : 'text-white'
+            }
+         to="/">Home</NavLink></li>
+        <li><NavLink 
+        className={({ isActive }) =>
+              isActive ? 'text-yellow-400 font-bold' : 'text-white'
+            }
+        to="/favorites">Favorites</NavLink></li>
       </ul>
     </nav>
   );
