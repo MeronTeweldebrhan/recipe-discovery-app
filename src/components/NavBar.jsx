723ddc95f62de6197e0eee81aaff6aac05f418be
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-30 22:41:45 -0400

    Update navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,48 +10,92 @@ function NavBar() {
     navigate(`/search/${searchTerm}`);
     setSearchTerm('');
   };
-
-  return (
-    <div>
-      <nav className='bg-gray-900 h-13 text-white shadow-md'>
-        <ul className='mt-2 flex space-x-8'>
-          <li className=' ms-10'>
+ return (
+  <>
+    <nav className="bg-gray-900 py-4 text-white shadow-md">
+      <div className="flex items-center justify-between w-11/12 mx-auto">
+        <h1 className="text-2xl font-bold">Posts Social</h1>
+        <ul className="flex space-x-6">
+          <li>
             <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-yellow-400 font-bold' : 'text-white'
-              }
               to="/"
+              className={({ isActive }) =>
+                isActive ? "text-yellow-400 font-bold" : "text-white"
+              }
             >
               Home
             </NavLink>
           </li>
           <li>
             <NavLink
-              className={({ isActive }) =>
-                isActive ? 'text-yellow-400 font-bold' : 'text-white'
-              }
               to="/favorites"
+              className={({ isActive }) =>
+                isActive ? "text-yellow-400 font-bold" : "text-white"
+              }
             >
               Favorites
             </NavLink>
           </li>
         </ul>
-      </nav>
-
-      <form onSubmit={handleSubmit} className="mt-4 ms-10">
-        <input
-          type='text'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className='ms-200 text-black p-2 rounded'
-          placeholder='Search recipes...'
-        />
-        <button type='submit' className='ml-2 bg-yellow-400 px-3 py-2 rounded'>
-          Search
-        </button>
-      </form>
-    </div>
+      </div>
+    </nav>
+    <form onSubmit={handleSubmit} className="mt-4 ms-10">
+   <input
+      type='text'
+      value={searchTerm}
+       onChange={(e) => setSearchTerm(e.target.value)}
+       className='ms-200 text-black p-2 rounded'
+       placeholder='Search recipes...'
+     />
+     <button type='submit' className='ml-2 bg-yellow-400 px-3 py-2 rounded'>
+       Search
+     </button>
+   </form>
+    </>
   );
 }
+//   return (
+//     // <div>
+//     //   <nav className="bg-gray-900 py-4 text-white shadow-md">
+//     //     <div>
+//     //     <ul className='mt-2 flex space-x-8'>
+//     //       <li className=' ms-10'>
+//     //         <NavLink
+//     //           className={({ isActive }) =>
+//     //             isActive ? 'text-yellow-400 font-bold' : 'text-white'
+//     //           }
+//     //           to="/"
+//     //         >
+//     //           Home
+//     //         </NavLink>
+//     //       </li>
+//     //       <li>
+//     //         <NavLink
+//     //           className={({ isActive }) =>
+//     //             isActive ? 'text-yellow-400 font-bold' : 'text-white'
+//     //           }
+//     //           to="/favorites"
+//     //         >
+//     //           Favorites
+//     //         </NavLink>
+//     //       </li>
+//     //     </ul>
+//     //   </nav>
+      
+//     //   <form onSubmit={handleSubmit} className="mt-4 ms-10">
+//     //     <input
+//     //       type='text'
+//     //       value={searchTerm}
+//     //       onChange={(e) => setSearchTerm(e.target.value)}
+//     //       className='ms-200 text-black p-2 rounded'
+//     //       placeholder='Search recipes...'
+//     //     />
+//     //     <button type='submit' className='ml-2 bg-yellow-400 px-3 py-2 rounded'>
+//     //       Search
+//     //     </button>
+//     //   </form>
+//     // </div>
+//   );
+// }
 
 export default NavBar;
